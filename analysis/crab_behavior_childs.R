5fe550e6a6bf2629def026f49844747c011699d7
Jchilds <mmerf24@gmail.com>
2019-11-26 14:33:38 -0500

    applied modifications to existing visualizations and added in new visualizations for "Per Trial"

--- analysis/crab_behavior_childs.R
+++ analysis/crab_behavior_childs.R
@@ -1,4 +1,4 @@
-# test
+#1) Loading data
 rm(list=ls())
 
 library(tidyverse)
@@ -12,6 +12,7 @@ time <- read.csv(here("data/condensed_crab_video_trial_datasheet.csv"))
 data <- tbl_df(crab)
 df2 <- tbl_df(time)
 
+#2) Summarizing data so that there is 1 row per crab
 df1 <- data %>%
   group_by(metadat_id) %>%
   summarize(sum_aggressive_posture = sum(aggressive_posture),
@@ -22,26 +23,35 @@ df1 <- data %>%
             sum_mussels_eaten = sum(mussels_eaten),
             sum_activity_subtotal = sum(activity_subtotal))
 
-# Integrating video_total into data
-joined <- left_join(df1, df2)
+#3) Integrating Video Total into data
+joined <- right_join(df1, df2)
 
-# Creating activity_proportion & total_aggression columns  
+#4) Creating Activity Proportion & Total Aggression columns  
 data_complete <- joined %>%
   group_by(metadat_id) %>%
   mutate(activity_proportion=sum_activity_subtotal/video_total,
          total_aggression=sum_aggressive_posture+sum_fights_initiated+sum_fights_responded+sum_claw_strikes)
 
-# Figures
-## Per Crab
-ggplot(data_complete, aes(x=crab_dens, y=activity_proportion))+geom_point()
+#5) Figures Per Crab
+## Density vs. Activity Plot
+ggplot(data_complete, aes(as.factor(crab_dens), activity_proportion))+geom_boxplot()+
+  labs(title="Density vs. Activity per Crab", x="Crab Density", y="Activity Proportion")
 
-ggplot(data_complete, aes(x=crab_dens, y=total_aggression))+geom_point()
+## Density vs. Total Aggression Plot
+ggplot(data_complete, aes(as.factor(crab_dens), total_aggression))+geom_boxplot()+
+  labs(title="Density vs. Aggression per Crab", x="Crab Density", y="Total Aggression")
 
-ggplot(data_complete, aes(x=activity_proportion, y=total_aggression, color=crab_dens))+geom_point()
+## Activity vs. Total Agression Plot
+ggplot(data_complete, aes(x=activity_proportion, y=total_aggression, color=as.factor(crab_dens)))+geom_point()+
+  geom_smooth(method = "lm")+
+  labs(title="Activity vs. Aggression per Crab", x="Activity Proportion", y="Total Aggression", color="Crab Density")
 
-ggplot(data_complete, aes(x=activity_proportion, y=sum_mussels_eaten, color=crab_dens))+geom_point()
+## Activity vs. Mussles Eaten Plot
+ggplot(data_complete, aes(x=activity_proportion, y=sum_mussels_eaten, color=as.factor(crab_dens)))+geom_point()+
+  geom_smooth(method = "lm")+
+  labs(title="Activity vs. Mussels Eaten per Crab", x="Activity Proportion", y="Mussels Eaten", color="Crab Density")
 
-# Per Trial
+#6) Replicating steps 2-4 Per Trial
 df3 <- data %>%
   group_by(trial_id) %>%
   summarize(sum_aggressive_posture = sum(aggressive_posture),
@@ -57,11 +67,22 @@ data_complete_trial <- joined_trial %>%
   mutate(activity_proportion=sum_activity_subtotal/video_total,
          total_aggression=sum_aggressive_posture+sum_fights_initiated+sum_fights_responded+sum_claw_strikes)
 
-## Figures
-ggplot(data_complete_trial, aes(x=crab_dens, y=activity_proportion))+geom_point()
+#7) Figures per Trial
+## Density vs. Activity Plot
+ggplot(data_complete_trial, aes(as.factor(crab_dens), activity_proportion))+geom_boxplot()+
+  labs(title="Density vs. Activity per Trial", x="Crab Density", y="Activity Proportion")
 
-ggplot(data_complete_trial, aes(x=crab_dens, y=total_aggression))+geom_point()
+## Density vs. Total Aggression Plot
+ggplot(data_complete_trial, aes(as.factor(crab_dens), total_aggression))+geom_boxplot()+
+  labs(title="Density vs. Aggression per Trial", x="Crab Density", y="Total Aggression")
 
-ggplot(data_complete_trial, aes(x=activity_proportion, y=total_aggression, color=crab_dens))+geom_point()
+## Activity vs. Total Agression Plot
+ggplot(data_complete_trial, aes(x=activity_proportion, y=total_aggression, color=as.factor(crab_dens)))+geom_point()+
+  geom_smooth(method = "lm")+
+  labs(title="Activity vs. Aggression per Trial", x="Activity Proportion", y="Total Aggression", color="Crab Density")
 
-ggplot(data_complete_trial, aes(x=activity_proportion, y=sum_mussels_eaten, color=crab_dens))+geom_point()
+## Activity vs. Mussles Eaten Plot
+ggplot(data_complete_trial, aes(x=activity_proportion, y=sum_mussels_eaten, color=as.factor(crab_dens)))+geom_point()+
+  geom_smooth(method = "lm")+
+  labs(title="Activity vs. Mussels Eaten per Trial", x="Activity Proportion", y="Mussels Eaten", color="Crab Density")
+
